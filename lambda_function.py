cabb3954d6d6ee7e6046330fd9aa975d012914c1
Lukasz <lukasz.kniola@gmail.com>
2019-01-10 11:21:40 +0000

    example lambda_function

---
+++ lambda_function.py
@@ -0,0 +1,44 @@
+import skill-utils
+
+class LaunchRequestHandler():
+    """Handler for Session Launched."""
+    def can_handle(self, req):
+        return req.isType("LaunchRequest")
+
+    def handle(self, req, res):
+        res.txt_set("LAUNCHING...")
+        return res.build()
+
+class CheckLetterHandler():
+    """Handler for Checking Letter"""
+    def can_handle(self, req):
+        return req.isIntent("getFirstLetter")
+
+    def handle(self, req, res):
+        res.txt_add("FIRST LETTER IS " + req.slots.names.code)
+        return res.build()
+
+class CatchAllHandler():
+    """Catch-all exception handler, respond with custom message."""
+    def can_handle(self, req):
+        return True
+
+    def handle(self, req, res):
+        res.txt_add("Sorry, I couldn't understand what you said. Please try again.")
+        return res.build()
+
+s = Skill()
+s.addHandler(LaunchRequestHandler())
+s.addHandler(CheckLetterHandler())
+s.addHandler(CatchAllHandler())
+lambda_handler = s.lambda_handler()
+
+print(lambda_handler(event,""))
+
+
+
+
+
+
+
+
